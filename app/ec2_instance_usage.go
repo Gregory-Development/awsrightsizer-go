fd25b7e3fa6a12711f606db78e436ca2069a6193
Jordan Gregory <gregory.jordan.m@gmail.com>
2019-04-29 09:48:05 -0500

    add a little more logic

--- app/ec2_instance_usage.go
+++ app/ec2_instance_usage.go
@@ -4,13 +4,34 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/costexplorer"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"log"
 	"os"
 )
 
-func CalculateEC2Usage() {
-	ctx := aws.Context(aws.BackgroundContext())
+type Instance struct {
+	Name string
+	ID string
+}
 
+var (
+	Instances []Instance
+	ctx = aws.Context(aws.BackgroundContext())
+)
+
+func GatherEC2Information() {
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(os.Getenv("AWS_REGION"))})
+	if err != nil {
+		log.Println(err)
+	}
+
+	svc := ec2.New(sess)
+
+	r, err := svc.DescribeInstancesWithContext(ctx, &ec2.DescribeInstancesInput{})
+
+}
+
+func CalculateEC2Usage() {
 	sess, err := session.NewSession(&aws.Config{Region: aws.String(os.Getenv("AWS_REGION"))})
 	if err != nil {
 		log.Println(err)
--- app/rds_instance_usage.go
+++ app/rds_instance_usage.go
@@ -1,1 +1,31 @@
 package app
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/rds"
+	"log"
+	"os"
+)
+
+type Instance struct {
+	Name string
+	ID string
+}
+
+var (
+	ctx = aws.Context(aws.BackgroundContext())
+	Instances []Instance
+)
+
+func GatherRDSInformation() {
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(os.Getenv("AWS_REGION"))})
+	if err != nil {
+		log.Println(err)
+	}
+
+	svc := rds.New(sess)
+
+	r, err := svc.DescribeDBInstancesWithContext(ctx, &rds.DescribeDBInstancesInput{})
+
+}
